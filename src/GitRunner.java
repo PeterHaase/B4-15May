8c87acd0dcb9395f88acf4b9ad9790fba5fafec9
compsci <petercwhaase@gmail.com>
2017-05-15 14:05:29 -0600

    k

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -4,6 +4,8 @@ public class GitRunner {
 	public static void main(String[] args) {
 		System.out.println("Initial Build");
 		System.out.println("Fix Null Pointer");
+		///work...
+		System.out.println("Stuff");
 	}
 
 }
